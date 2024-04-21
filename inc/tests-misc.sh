56dfc785465efb14a18dec7e5bd8281669699bb7
Thomas Kieslich <thomaskieslich@users.noreply.github.com>
2024-04-21 12:56:39 +0200

    Add Tests for Misc Files.

---
+++ inc/tests-misc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+### Misc Tests ###
+lint:composer() {
+    echo -e "\n ####################### \n # Composer Validation # \n ####################### \n"
+    composer validate --working-dir "${PACKAGE_PATH}/${PACKAGE_NAME}" --with-dependencies --strict || EXIT_CODE=$?
+    echo "${PACKAGE_NAME} Composer is Validated."
+    return $EXIT_CODE
+}
+
+lint:json() {
+    echo -e "\n ################# \n # Json Linting # \n ################# \n"
+    find "${PACKAGE_PATH}/${PACKAGE_NAME}" ! -path "*/node_modules/*" -name "*.json" -print0 | \
+        xargs --null -r php "${TEST_PATH}/vendor/bin/jsonlint" || EXIT_CODE=$?
+    echo "Json is Linted."
+    return $EXIT_CODE
+}
+
+lint:md() {
+    echo -e "\n ################ \n # Markdown Linting # \n ################ \n"
+    npx markdownlint-cli2 "${PACKAGE_PATH}/${PACKAGE_NAME}/**/*.md" \
+        --config "${CONFIGURATION_PATH}/.markdownlint-cli2.yaml" || EXIT_CODE=$?
+    echo "Markdown is Linted Exit."
+    return $EXIT_CODE
+}
+
+lint:yaml() {
+    echo -e "\n ################# \n # Yaml Linting # \n ################# \n"
+    find "${PACKAGE_PATH}/${PACKAGE_NAME}" ! -path "*/node_modules/*" -name "*.y*ml" -print0 | \
+        xargs --null -r php "${TEST_PATH}/vendor/bin/yaml-lint" --ansi || EXIT_CODE=$?
+    echo "Yaml is Linted"
+    return $EXIT_CODE
+}
--- t3static.sh
+++ t3static.sh
@@ -14,7 +14,7 @@ TYPO3SCAN_TARGET=12
 
 # Override Defaults
 if [ -f ${BASH_SOURCE%/*}/.env ]; then
-  source "${BASH_SOURCE%/*}/.env"
+    source "${BASH_SOURCE%/*}/.env"
 fi
 
 CONFIGURATION_PATH=${CONFIGURATION_PATH}
@@ -28,75 +28,94 @@ TYPO3SCAN_TARGET=${TYPO3SCAN_TARGET}
 # Include Test Scripts
 source "${BASH_SOURCE%/*}/inc/tests-frontend.sh"
 source "${BASH_SOURCE%/*}/inc/tests-php.sh"
+source "${BASH_SOURCE%/*}/inc/tests-misc.sh"
 
 # Get Options
 while getopts "p:t:" option; do
-  case $option in
-  p)
-    PACKAGE_NAME=$OPTARG
-    ;;
-  t)
-    TEST_TYPE=$OPTARG
-    ;;
-  *) ;;
-  esac
+    case $option in
+    p)
+        PACKAGE_NAME=$OPTARG
+        ;;
+    t)
+        TEST_TYPE=$OPTARG
+        ;;
+    *) ;;
+    esac
 done
 
 ## Init Packages
 install:packages() {
-  cd ${TEST_PATH} || exit
-  composer install --no-progress --no-interaction
-  npm install --quiet
-  cd ../..
+    cd ${TEST_PATH} || exit
+    composer install --no-progress --no-interaction
+    npm install --quiet
+    cd ../..
 }
 
 # Get Test
 case "${TEST_TYPE}" in
 install)
-  install:packages
-  ;;
+    install:packages
+    ;;
 css)
-  lint:css
-  ;;
+    lint:css
+    ;;
 css-fix)
-  lint:css:fix
-  ;;
+    lint:css:fix
+    ;;
 scss)
-  lint:scss
-  ;;
+    lint:scss
+    ;;
 scss-fix)
-  lint:scss:fix
-  ;;
+    lint:scss:fix
+    ;;
 js)
-  lint:js
-  ;;
+    lint:js
+    ;;
 js-fix)
-  lint:js:fix
-  ;;
+    lint:js:fix
+    ;;
 frontend)
-  lint:scss
-  lint:js
-  ;;
+    lint:scss
+    lint:js
+    ;;
 php-cs)
-  php:cs
-  ;;
+    php:cs
+    ;;
 php-cs-fix)
-  php:cs:fix
-  ;;
+    php:cs:fix
+    ;;
 php-stan)
-  php:stan
-  ;;
-backend)
-  php:cs
-  php:stan
-  ;;
+    php:stan
+    ;;
+php)
+    php:cs
+    php:stan
+    ;;
+composer)
+    lint:composer
+    ;;
+json)
+    lint:json
+    ;;
+md)
+    lint:md
+    ;;
+yaml)
+    lint:yaml
+    ;;
+misc)
+    lint:composer
+    lint:json
+    lint:md
+    lint:yaml
+    ;;
 all)
-  lint:scss
-  lint:js
-  php:cs
-  php:stan
-  ;;
+    lint:scss
+    lint:js
+    php:cs
+    php:stan
+    ;;
 *)
-  echo "use parameters (all | json | yaml | typoscript | scss | js | …)"
-  ;;
+    echo "use parameters (all | json | yaml | typoscript | scss | js | …)"
+    ;;
 esac
