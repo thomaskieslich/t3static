f8b4fad726093961c5b1c751bd43f585e0db5b71
Thomas Kieslich <thomaskieslich@users.noreply.github.com>
2024-12-10 20:48:02 +0100

    php-cs-fixer, set ParallelConfig and extra comment rule

--- config/php-cs-fixer/coding-standards.php
+++ config/php-cs-fixer/coding-standards.php
@@ -3,7 +3,12 @@
 $config = \TYPO3\CodingStandards\CsFixerConfig::create();
 $config->getFinder()
     ->ignoreVCSIgnored(true)
-    ->in(realpath(__DIR__ . '/../../'))
-;
+    ->in(realpath(__DIR__ . '/../../'));
+
+$config->setParallelConfig(new PhpCsFixer\Runner\Parallel\ParallelConfig(4, 20));
+
+$config->addRules([
+    'single_line_comment_spacing' => true,
+]);
 
 return $config;
