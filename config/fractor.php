a1ab912a77d71652ad5b517b4ac178518e1e0e46
Thomas Kieslich <thomas.kieslich@pluswerk.ag>
2025-08-08 20:06:17 +0200

    fractor: Set and Export FULL_PACKAGE_PATH

--- config/fractor.php
+++ config/fractor.php
@@ -3,16 +3,7 @@ use a9f\Fractor\Configuration\FractorConfiguration;
 use a9f\Typo3Fractor\Set\Typo3LevelSetList;
 
 return FractorConfiguration::configure()
-    ->withPaths([__DIR__ . '/packages'])
-    ->withSkip([
-        __DIR__ . '**/Build/node_modules/**',
-        __DIR__ . '**/Resources/Public/bower_components/**',
-        __DIR__ . '**/Resources/Public/vendor/**',
-        __DIR__ . '**/Resources/Private/Schema/**',
-        __DIR__ . '**/Resources/Private/Foundation/**', // dv-matthies-pagetemplate
-        __DIR__ . '/vendor/davitec/pt-extlist/Documentation/**',
-        __DIR__ . '/vendor/davitec/restler/Documentation/**',
-    ])
+    ->withPaths([getenv('FULL_PACKAGE_PATH')])
     ->withSets([
-        Typo3LevelSetList::UP_TO_TYPO3_13
+        Typo3LevelSetList::UP_TO_TYPO3_12
     ]);
