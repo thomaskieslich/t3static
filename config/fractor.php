8a86d693197e5c55e2d694ca4fb8844d58c8f60a
Thomas Kieslich <thomaskieslich@users.noreply.github.com#>
2025-10-05 21:02:44 +0200

    t3static: add node_modules and vendor to skip Paths

--- config/fractor.php
+++ config/fractor.php
@@ -4,6 +4,12 @@ use a9f\Typo3Fractor\Set\Typo3LevelSetList;
 
 return FractorConfiguration::configure()
     ->withPaths([getenv('FULL_PACKAGE_PATH')])
+    ->withSkip([
+        '*/node_modules/*',
+        '*/vendor/*',
+        '**/node_modules/*',
+        '**/vendor/*',
+    ])
     ->withSets([
-        Typo3LevelSetList::UP_TO_TYPO3_12
+        Typo3LevelSetList::UP_TO_TYPO3_13
     ]);
