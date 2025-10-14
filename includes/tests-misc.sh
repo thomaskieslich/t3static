af84bbe0f681cc22800f0793ed31613698060a28
Thomas Kieslich <thomaskieslich@users.noreply.github.com>
2025-10-14 21:51:19 +0200

    Rename Functions

--- includes/tests-misc.sh
+++ includes/tests-misc.sh
@@ -3,25 +3,25 @@
 ### Misc Tests ###
  composer() {
      echoTestHeader "Composer Validation"
-     command composer validate --working-dir "${FULL_PACKAGE_PATH}" --with-dependencies --strict || local EXIT_CODE=$?
-     echoTestFooter "Composer is Validated." ${EXIT_CODE}
-     return ${EXIT_CODE}
+     command composer validate --working-dir "${FULL_PACKAGE_PATH}" --with-dependencies --strict || EXIT_CODE=$?
+     echoTestFooter "${PACKAGE_NAME} Composer is Validated."
+     return "${EXIT_CODE}"
  }
 
 json() {
     echoTestHeader "Json Linting"
     find "${FULL_PACKAGE_PATH}" ! -path "*/node_modules/*" -name "*.json" -print0 |
-        xargs --null -r php "${TEST_PATH}/vendor/bin/jsonlint" || local EXIT_CODE=$?
-    echoTestFooter "Json is Linted." ${EXIT_CODE}
-    return ${EXIT_CODE}
+        xargs --null -r php "${TEST_PATH}/vendor/bin/jsonlint" || EXIT_CODE=$?
+    echoTestFooter "Json is Linted."
+    return "${EXIT_CODE}"
 }
 
 md() {
     echoTestHeader "Markdown Linting"
     npx --prefix "${TEST_PATH}" markdownlint-cli2 "${FULL_PACKAGE_PATH}/**/*.md" \
-        --config "${CONFIGURATION_PATH}/.markdownlint-cli2.yaml" || local EXIT_CODE=$?
-    echoTestFooter "Markdown is Linted " ${EXIT_CODE}
-    return ${EXIT_CODE}
+        --config "${CONFIGURATION_PATH}/.markdownlint-cli2.yaml" || EXIT_CODE=$?
+    echoTestFooter "Markdown is Linted Exit."
+    return "${EXIT_CODE}"
 }
 
 md-fix() {
@@ -29,14 +29,14 @@ md-fix() {
     npx --prefix "${TEST_PATH}" markdownlint-cli2 --fix "${FULL_PACKAGE_PATH}/**/*.md" \
         --config "${CONFIGURATION_PATH}/.markdownlint-cli2.yaml"
     local EXIT_CODE=$?
-    echoTestFooter "Markdown is Linted Exit." ${EXIT_CODE}
-    return ${EXIT_CODE}
+    echoTestFooter "Markdown is Linted Exit."
+    return "${EXIT_CODE}"
 }
 
 yaml() {
     echoTestHeader "Yaml Linting"
     find "${FULL_PACKAGE_PATH}" ! -path "*/node_modules/*" -name "*.y*ml" -print0 |
-        xargs --null -r php "${TEST_PATH}/vendor/bin/yaml-lint" --ansi || local EXIT_CODE=$?
-    echoTestFooter "Yaml is Linted" ${EXIT_CODE}
-    return ${EXIT_CODE}
+        xargs --null -r php "${TEST_PATH}/vendor/bin/yaml-lint" --ansi || EXIT_CODE=$?
+    echoTestFooter "Yaml is Linted"
+    return $EXIT_CODE
 }
