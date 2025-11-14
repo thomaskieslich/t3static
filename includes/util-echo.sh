25947dc6aad17892403d9b70916a100c3f6bf577
Thomas Kieslich <thomaskieslich@users.noreply.github.com>
2025-11-14 22:19:24 +0100

    Add EXIT_CODES to echoTestFooter

--- includes/util-echo.sh
+++ includes/util-echo.sh
@@ -2,7 +2,6 @@
 
 # several functions to output echos in more visual way
 
-
 # Example (color blue):
 # ==> Test: rector
 echoInfo() {
@@ -24,5 +23,23 @@ echoTestHeader() {
 # Example (color magenta):
 # ==> Rector completed
 echoTestFooter() {
-    echo -e "\n\033[1;35m==> $1\033[0m\n"
+    if [[ -n "$2" ]]; then
+        local exit_message=""
+        local output=""
+        case "$2" in
+        1) exit_message="General error" ;;
+        2) exit_message="Misuse of shell builtin" ;;
+        126) exit_message="Command cannot be executed" ;;
+        127) exit_message="Command not found" ;;
+        130) exit_message="Script interrupted by user (CTRL+C)" ;;
+        *) exit_message="Unknown error" ;;
+        esac
+        local color="\033[1;31m" # Red for errors
+        output=$(echo -e "${color}EXIT_CODE: $2 - $exit_message\033[0m")
+    else
+        local color="\033[1;32m" # Green for success
+        output=$(echo -e "${color}Success\033[0m")
+    fi
+
+    echo -e "\n\033[1;35m==> $1: $output\033[0m\n"
 }
