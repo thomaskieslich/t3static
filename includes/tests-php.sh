cbd0e346e51de9b1e2bad94732f4dc0922b6059b
Thomas Kieslich <thomaskieslich@users.noreply.github.com>
2025-12-31 01:11:28 +0100

    Update PHP-CS-Fixer configuration.

--- includes/tests-php.sh
+++ includes/tests-php.sh
@@ -3,10 +3,12 @@
 ### PHP Tests ###
 php-cs() {
     echoTestHeader "php:cs"
+    EXIT_CODE=0
     "${TEST_PATH}/vendor/bin/php-cs-fixer" \
         --diff \
         --dry-run \
         --using-cache=no \
+        --quiet \
         --config="${CONFIGURATION_PATH}/php-cs-fixer/coding-standards.php" \
         fix "${FULL_PACKAGE_PATH}" || EXIT_CODE=$?
     echoTestFooter "php:cs completed" "${EXIT_CODE}"
@@ -15,9 +17,11 @@ php-cs() {
 
 php-cs-fix() {
     echoTestHeader "php:cs:fix"
+    EXIT_CODE=0
     "${TEST_PATH}/vendor/bin/php-cs-fixer" \
         --diff \
         --using-cache=no \
+        --quiet \
         --config="${CONFIGURATION_PATH}/php-cs-fixer/coding-standards.php" \
         fix "${FULL_PACKAGE_PATH}" || EXIT_CODE=$?
     echoTestFooter "php:cs:fix completed" "${EXIT_CODE}"
@@ -26,7 +30,10 @@ php-cs-fix() {
 
 php-stan() {
     echoTestHeader "php:stan"
-    "${TEST_PATH}/vendor/bin/phpstan" analyse --level "${PHPSTAN_LEVEL}" \
+    EXIT_CODE=0
+    "${TEST_PATH}/vendor/bin/phpstan" analyse \
+        --level "${PHPSTAN_LEVEL}" \
+        --memory-limit=512M \
         --configuration "${CONFIGURATION_PATH}/php-stan/phpstan.neon" \
         --autoload-file "vendor/autoload.php" \
         "${FULL_PACKAGE_PATH}" || EXIT_CODE=$?
@@ -36,7 +43,8 @@ php-stan() {
 }
 
 php-stan-baseline() {
-    echoTestHeader "php:stan"
+    echoTestHeader "php:stan:baseline"
+    EXIT_CODE=0
     "${TEST_PATH}/vendor/bin/phpstan" analyse --level "${PHPSTAN_LEVEL}" \
         --configuration "${CONFIGURATION_PATH}/php-stan/phpstan.neon" \
         --autoload-file "vendor/autoload.php" \
