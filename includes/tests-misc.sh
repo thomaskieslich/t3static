9aa7527fb2414571b6754efe6cd71d6e3b0e9582
Thomas Kieslich <thomaskieslich@users.noreply.github.com>
2026-01-26 22:57:39 +0100

    Update composer validation

--- includes/tests-misc.sh
+++ includes/tests-misc.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
 
 ### Misc Tests ###
- composer() {
-     echoTestHeader "Composer Validation"
-     command composer validate --working-dir "${FULL_PACKAGE_PATH}" --with-dependencies --strict || EXIT_CODE=$?
-     echoTestFooter "${PACKAGE_NAME} Composer is Validated."
-     return "${EXIT_CODE}"
- }
+composer-validate() {
+    echoTestHeader "Composer Validation"
+    EXIT_CODE=0
+    command composer \
+        --working-dir="${FULL_PACKAGE_PATH}" \
+        validate \
+        --strict || EXIT_CODE=$?
+    echoTestFooter "${PACKAGE_NAME} Composer is Validated."
+    return "${EXIT_CODE}"
+}
+
+composer-normalize() {
+    echoTestHeader "Composer Normalize"
+    EXIT_CODE=0
+    cd "${TEST_PATH}" || exit 1
+    command composer normalize \
+        --dry-run \
+        --indent-style tab \
+        --indent-size 1 \
+        --no-interaction \
+        "${FULL_PACKAGE_PATH}/composer.json" ||
+        EXIT_CODE=$?
+    echoTestFooter "${PACKAGE_NAME} Composer is Normalized. (DRY)"
+    return "${EXIT_CODE}"
+}
+
+composer-normalize-fix() {
+    echoTestHeader "Composer Normalize Fix"
+    EXIT_CODE=0
+    cd "${TEST_PATH}" || exit 1
+    command composer normalize \
+        --indent-style tab \
+        --indent-size 1 \
+        --no-interaction \
+        "${FULL_PACKAGE_PATH}/composer.json" ||
+        EXIT_CODE=$?
+    echoTestFooter "${PACKAGE_NAME} Composer is Normalized."
+    return "${EXIT_CODE}"
+}
 
 json() {
     echoTestHeader "Json Linting"
+    EXIT_CODE=0
     find "${FULL_PACKAGE_PATH}" ! -path "*/node_modules/*" -name "*.json" -print0 |
         xargs --null -r php "${TEST_PATH}/vendor/bin/jsonlint" || EXIT_CODE=$?
     echoTestFooter "Json is Linted."
@@ -18,6 +52,7 @@ json() {
 
 md() {
     echoTestHeader "Markdown Linting"
+    EXIT_CODE=0
     npx --prefix "${TEST_PATH}" markdownlint-cli2 "${FULL_PACKAGE_PATH}/**/*.md" \
         --config "${CONFIGURATION_PATH}/.markdownlint-cli2.yaml" || EXIT_CODE=$?
     echoTestFooter "Markdown is Linted Exit."
@@ -33,6 +68,7 @@ md-fix() {
 
 yaml() {
     echoTestHeader "Yaml Linting"
+    EXIT_CODE=0
     find "${FULL_PACKAGE_PATH}" ! -path "*/node_modules/*" -name "*.y*ml" -print0 |
         xargs --null -r php "${TEST_PATH}/vendor/bin/yaml-lint" --ansi || EXIT_CODE=$?
     echoTestFooter "Yaml is Linted"
